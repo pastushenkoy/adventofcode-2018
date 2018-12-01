ba71b6a50f083636db5d28e2420b8dca7e2d8cb5
pastushenkoy <pastushenkoy@gmail.com>
2018-12-02 00:21:19 +0300

    gitignore added

--- day01/main.go
+++ day01/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
@@ -11,8 +14,40 @@ func check(e error) {
 	}
 }
 
-func main() {
-	dat, err := ioutil.ReadFile("/input.txt")
+func readFile(input string) []string {
+	inputFile, err := filepath.Abs(input);
 	check(err)
-	fmt.Print(string(dat))
+	dat, err := ioutil.ReadFile(inputFile)
+	check(err)
+	return strings.Split(string(dat), "\r\n")
+}
+
+func main() {
+	data :=readFile("input.txt")
+	var freq []int
+	sum := 0
+	for _, st := range data{
+		num, _ := strconv.Atoi(st)
+		freq = append(freq, num)
+		sum += num
+	}
+	fmt.Println(sum)
+
+	hashset := make(map[int]struct{})
+
+	currentFreq := 0
+	hashset[currentFreq] = struct{}{}
+	i:=0
+	for {
+		currentFreq += freq[i%len(freq)]
+		if _, hasValue := hashset[currentFreq]; hasValue{
+			break;
+		}
+
+		hashset[currentFreq] = struct{}{}
+		i++
+	}
+
+	fmt.Println(currentFreq)
+	fmt.Print(i)
 }
