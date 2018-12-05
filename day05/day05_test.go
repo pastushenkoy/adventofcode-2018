789307d67a6d4cb91d9f5d9d4a74a165380fd684
pastushenkoy <pastushenkoy@gmail.com>
2018-12-06 00:09:09 +0300

    day 5 solved

--- day05/day05_test.go
+++ day05/day05_test.go
@@ -1,1 +1,15 @@
 package main
+
+import "testing"
+
+func TestTask01(t *testing.T) {
+	if getSurvivedPolymersCount("dabAcCaCBAcCcaDA") != 10 {
+		t.Fail()
+	}
+}
+
+func TestTask02(t *testing.T) {
+	if getShortestPolymerLength("dabAcCaCBAcCcaDA") != 4 {
+		t.Fail()
+	}
+}
--- day05/main.go
+++ day05/main.go
@@ -1,1 +1,85 @@
-package day05
+package main
+
+import (
+	"fmt"
+	"github.com/pastushenkoy/adventOfCode/utils"
+	"strings"
+)
+
+func main() {
+	data := utils.ReadStringFromFile("input.txt")
+
+	fmt.Println("The length of the rest string is ", getSurvivedPolymersCount(data))
+
+	fmt.Println("The length of the rest string after excluging most recent char is ", getShortestPolymerLength(data))
+}
+
+func getSurvivedPolymersCount(data string) int {
+	runes := []rune(data)
+
+	for allSurvived := false; !allSurvived; {
+		allSurvived = true
+		for i := 0; i < len(runes)-1; {
+			currentRune := runes[i]
+			if currentRune == '_' {
+				i++
+				continue
+			}
+			nextIndex, exists := findNextRune(runes, i)
+			if !exists {
+				break
+			}
+			nextRune := runes[nextIndex]
+
+			if destroyed(currentRune, nextRune) {
+				runes[i] = '_'
+				runes[nextIndex] = '_'
+				i = nextIndex + 1
+				allSurvived = false
+			} else {
+				i++
+			}
+
+		}
+	}
+
+	runeCount := 0
+	for _, r := range runes {
+		if r != '_' {
+			runeCount++
+		}
+	}
+
+	return runeCount
+}
+
+func findNextRune(runes []rune, currentRuneIndex int) (nextIndex int, exists bool) {
+	for i := currentRuneIndex + 1; i < len(runes); i++ {
+		if runes[i] != '_' {
+			return i, true
+		}
+	}
+	return 0, false
+}
+
+func destroyed(r1 rune, r2 rune) bool {
+	return r1 != '_' && r1 != r2 && strings.ToLower(string(r1)) == strings.ToLower(string(r2))
+}
+
+func getShortestPolymerLength(data string) int {
+	shortestPolymerLength := len(data)
+	for i := 97; i <= 122; i++ {
+		lower := string(i)
+		upper := strings.ToUpper(lower)
+
+		tempData := strings.Replace(data, lower, "_", -1)
+		tempData = strings.Replace(tempData, strings.ToUpper(upper), "_", -1)
+		length := getSurvivedPolymersCount(tempData)
+		if length < shortestPolymerLength{
+			shortestPolymerLength = length
+		}
+
+	}
+
+	return shortestPolymerLength
+}
